c134889c543a386561e56c3cfc0c27e9ea54c16a
kuojustin96 <justinkuo3904@yahoo.com>
2018-01-11 23:29:26 -0600

    Added simple menu system with various game settings

---
+++ Capstone/Assets/FadeManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeManager : MonoBehaviour
+{
+    public static FadeManager instance = null;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void FadeIn(CanvasGroup c, float _time, float _alpha = 1f)
+    {
+        StartCoroutine(iFadeIn(c, _time, _alpha));
+    }
+
+    IEnumerator iFadeIn(CanvasGroup objToFade, float _time, float _alpha = 1f)
+    {
+        while (objToFade.alpha < _alpha)
+        {
+            objToFade.alpha += Time.deltaTime / _time;
+            yield return null;
+        }
+
+        objToFade.alpha = _alpha;
+    }
+
+    public void FadeOut(CanvasGroup c, float _time, float _alpha = 0f)
+    {
+        StartCoroutine(iFadeOut(c, _time, _alpha));
+    }
+
+	IEnumerator iFadeOut(CanvasGroup objToFade, float _time, float _alpha = 0f)
+    {
+        while (objToFade.alpha > _alpha)
+        {
+            objToFade.alpha -= Time.deltaTime / _time;
+            yield return null;
+        }
+
+        objToFade.alpha = _alpha;
+    }
+
+    public void ChangeMenuPanels(CanvasGroup currentPanel, CanvasGroup newPanel, float time)
+    {
+        StartCoroutine(iChangeMenuPanels(currentPanel, newPanel, time));
+    }
+
+    IEnumerator iChangeMenuPanels(CanvasGroup currentPanel, CanvasGroup newPanel, float time)
+    {
+        while (currentPanel.alpha > 0)
+        {
+            currentPanel.alpha -= Time.deltaTime / (time / 2);
+            yield return null;
+        }
+
+        currentPanel.alpha = 0;
+        CanvasGroupOFF(currentPanel, true);
+
+        while(newPanel.alpha < 1)
+        {
+            newPanel.alpha += Time.deltaTime / (time / 2);
+            yield return null;
+        }
+
+        newPanel.alpha = 1;
+        CanvasGroupON(newPanel, true);
+    }
+
+    public void CanvasGroupOFF(CanvasGroup c, bool a)
+    {
+        c.interactable = false;
+        c.blocksRaycasts = false;
+        if (a) c.alpha = 0;
+    }
+
+    public void CanvasGroupON(CanvasGroup c, bool a)
+    {
+        c.interactable = true;
+        c.blocksRaycasts = true;
+        if (a) c.alpha = 1;
+    }
+}
---
+++ Capstone/Assets/MusicManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Music
+{
+    AxhomFissionInstrumental = 0,
+    AxhomOrbitInstrumental = 1,
+    FeelinGood = 2,
+    GetHappy = 3,
+    HappyAlley = 4,
+    HappyMusic1 = 5,
+    LoveLife = 6,
+	ZooMusic = 7,
+    MainZooMusic = 8,
+    MainMenu = 9,
+}
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager instance = null;
+
+	public Music music;
+
+    public List<AudioClip> SFXList = new List<AudioClip>();
+
+    private AudioSource auds;
+
+    // Use this for initialization
+    void Awake () {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (this.gameObject);
+		}
+
+        if (!GetComponent<AudioSource>())
+        {
+            auds = gameObject.AddComponent<AudioSource>();
+        }
+        else
+        {
+            auds = GetComponent<AudioSource>();
+        }
+
+		playMusic (music, 0.5f, true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playMusic(Music music, float volume = 0.5f, bool loop = false)
+    {
+        auds.volume = volume;
+        auds.loop = loop;
+
+        auds.clip = SFXList[(int)music];
+        auds.Play();
+    }
+
+	public void fadeMusic(float targetVolume = 0.2f, float fadeTime = 1f){
+		StartCoroutine (fadeMusicCoroutine (targetVolume, fadeTime));
+	}
+
+	private IEnumerator fadeMusicCoroutine(float targetVolume, float fadeTime){
+		bool under = true;
+
+		if (targetVolume > auds.volume) {
+			under = false;
+		}
+
+		while (auds.volume != targetVolume) {
+			auds.volume = Mathf.MoveTowards (auds.volume, targetVolume, fadeTime * Time.deltaTime);
+			Debug.Log (auds.volume);
+			yield return new WaitForEndOfFrame ();
+
+			if (under) {
+				if (auds.volume < targetVolume) {
+					auds.volume = targetVolume;
+				}
+			} else {
+				if (auds.volume > targetVolume) {
+					auds.volume = targetVolume;
+				}
+			}
+		}
+
+		auds.volume = targetVolume;
+	}
+
+    public void stopMusic(Music music)
+    {
+        foreach (AudioSource a in gameObject.GetComponents<AudioSource>())
+        {
+            if (a.clip == SFXList[(int)music])
+            {
+                if (a.isPlaying)
+                {
+                    a.Stop();
+                    a.clip = null;
+                }
+            }
+        }
+    }
+}
