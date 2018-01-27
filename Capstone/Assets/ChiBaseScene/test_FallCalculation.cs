ca41e4d94bf6f89d536eb3841523ab8993e2b912
Chofmann512 <gymchris123@gmail.com>
2018-01-26 19:29:55 -0600

    Chi's_Scene

---
+++ Capstone/Assets/ChiBaseScene/test_FallCalculation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_FallCalculation : MonoBehaviour {
+
+	Rigidbody rb;
+	public float fallThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float downVel = Mathf.Abs(rb.velocity.y);
+		Debug.Log("downVel " + downVel);
+		if(downVel > fallThreshold)
+		{
+			//Fall stun duration calculation
+			float rbVelHalf = downVel / 2;
+			float x = 1 + (downVel - fallThreshold);
+			float stunDur = (rbVelHalf + (downVel / x)) - fallThreshold;
+			Debug.Log(stunDur);
+		}
+	}
+}
