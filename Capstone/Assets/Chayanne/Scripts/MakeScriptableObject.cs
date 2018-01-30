1c645dcbfa44d36018b7cfddc3960a7c3d56d7e4
Chofmann512 <gymchris123@gmail.com>
2018-01-29 18:18:12 -0600

    Prototype
    Prototype Folder for testing

--- Capstone/Assets/Chayanne/Scripts/MakeScriptableObject.cs
+++ Capstone/Assets/Chayanne/Scripts/MakeScriptableObject.cs
@@ -1,22 +1,22 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
-
-namespace ckp
-{
-    public class MakeScriptableObject
-    {
-        [MenuItem("Assets/Create/Game Settings")]
-        public static void CreateMyAsset()
-        {
-            GameSettingsSO asset = ScriptableObject.CreateInstance<GameSettingsSO>();
-
-            AssetDatabase.CreateAsset(asset, "Assets/GameSettings.asset");
-            AssetDatabase.SaveAssets();
-
-            EditorUtility.FocusProjectWindow();
-
-            Selection.activeObject = asset;
-        }
-    }
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
+//
+//namespace ckp
+//{
+//    public class MakeScriptableObject
+//    {
+//        [MenuItem("Assets/Create/Game Settings")]
+//        public static void CreateMyAsset()
+//        {
+//            GameSettingsSO asset = ScriptableObject.CreateInstance<GameSettingsSO>();
+//
+//            AssetDatabase.CreateAsset(asset, "Assets/GameSettings.asset");
+//            AssetDatabase.SaveAssets();
+//
+//            EditorUtility.FocusProjectWindow();
+//
+//            Selection.activeObject = asset;
+//        }
+//    }
+//}
---
+++ Capstone/Assets/Prototype/Event_ButtonPlayerDetection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Event_ButtonPlayerDetection : NetworkBehaviour {
+
+	private Event_2PlayerDoorOpen doorOpen;
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+		doorOpen = door.GetComponent<Event_2PlayerDoorOpen>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "NetPlayer")
+			doorOpen.UpdatePlayersOnButton(1);
+		Debug.Log ("PlayerOnButton");	
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "NetPlayer")
+			doorOpen.UpdatePlayersOnButton(-1);
+	}
+}
