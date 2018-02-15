a1a311c6f7641a99d0c04c4af7f28fd905924672
Chofmann512 <gymchris123@gmail.com>
2018-02-14 19:55:43 -0600

    LightTriggers
    Event Finally works with triggers

--- Capstone/Assets/Prototype/net_list.cs
+++ Capstone/Assets/Prototype/net_list.cs
@@ -2,30 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using ckp;
 
+[RequireComponent(typeof(net_Event_MultiplayerTrigger))]
 public class net_list : NetworkBehaviour {
 	public List<GameObject> buttons = new List<GameObject>();
 	public  List<int>  buttonBool = new List<int>();
 	public static int triggersActive;
 	public int iterations;
+
+	private net_Event_MultiplayerTrigger eventTrigger;
+
+	void Start(){
+		eventTrigger = GetComponent<net_Event_MultiplayerTrigger> ();
+		randomizeTwo ();
+
+
+
+		//If you get down here, then you know all the correct buttons are pressed
+	}
+
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown("Fire1")){
-			Debug.Log ("serverCheck");
-			if (isServer) {
-				//if(triggersActive == 3){
-				Debug.Log ("new");
-				int count = 0;
-				string listString = "";
-				while (count < 5) {
-					buttonBool [count] = (int)Random.Range (0, 2);	
-					listString = listString + buttonBool [count].ToString ();
-					count++;
-				}
-					RpcColorRandomize (listString);
-				//}
-			}
-		}
+	void Update (){
+		
+
+
 	}
 	[ClientRpc]
 	public void RpcColorRandomize(string x){
@@ -43,5 +44,48 @@ public class net_list : NetworkBehaviour {
 				counta++;
 			}
 		}
+	}
+
+	public void randomizeTwo(){
+
+		Debug.Log ("Randomize");
+		if (isServer) {
+			//if(triggersActive == 3){
+
+			int count = 0;
+			int actives = 0;
+			string listString = "";
+			while (count < 5) {
+				if (actives == 2) {
+					buttonBool[count] = 0;
+				} else {
+					buttonBool [count] = (int)Random.Range (0, 2);
+				}
+				listString = listString + buttonBool [count].ToString ();
+				if (buttonBool [count] == 1) {
+					actives++;
+				}
+				count++;
+			}
+			if (actives == 2) {
+				RpcColorRandomize (listString);
+			}
+			else{
+				randomizeTwo ();
+			}
+			//}
+
+		}
+	}
+	public void checkCompletion(){
+
+		Debug.Log ("completion method");
+		for(int x = 0; x < buttonBool.Count; x++) {
+			if (buttonBool[x] == 1) {
+				if (!eventTrigger.triggers [x].IsTriggered())
+					return;
+			}
+		}
+		randomizeTwo ();
 	}
 }
