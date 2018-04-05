d13e0133c7e92debadd9b9ee795a6183045838aa
kuojustin96 <justinkuo3904@yahoo.com>
2018-04-04 21:48:08 -0500

    Added MusicManager and SFXManager

---
+++ Capstone/Assets/JumpPad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using jkuo;
+
+public class JumpPad : MonoBehaviour {
+
+    public float jumpForce = 50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "NetPlayer")
+        {
+            //Change this so that it just calls a single function
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * (jumpForce * 10), ForceMode.Impulse);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "NetPlayer")
+            other.GetComponent<net_PlayerController>().canJump = false;
+    }
+}
--- Capstone/Assets/Justin's Items and Player Setup/Main Menu/MusicManager.cs
+++ Capstone/Assets/Justin's Items and Player Setup/Main Menu/MusicManager.cs
@@ -2,29 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum MusicMainTrack
-{
-    AxhomFissionInstrumental = 0,
-    AxhomOrbitInstrumental = 1,
-    FeelinGood = 2,
-    GetHappy = 3,
-    HappyAlley = 4,
-    HappyMusic1 = 5,
-    LoveLife = 6,
-	ZooMusic = 7,
-    MainZooMusic = 8,
-    MainMenu = 9,
-}
-
 public class MusicManager : MonoBehaviour {
 
     public static MusicManager instance = null;
 
-	public MusicMainTrack music;
+    [System.Serializable]
+    public class MusicTracks
+    {
+        public string musicName;
+        [HideInInspector] public Object[] musicList;
+        public Dictionary<string, AudioClip> musicComponents = new Dictionary<string, AudioClip>();
+        public Dictionary<string, AudioSource> componentsPlaying = new Dictionary<string, AudioSource>();
+    }
 
-    public List<AudioClip> SFXList = new List<AudioClip>();
+    public MusicTracks[] Music;
 
-    private AudioSource auds;
+    private AudioSource mainTrackAuds;
 
     // Use this for initialization
     void Awake () {
@@ -36,68 +29,161 @@ public class MusicManager : MonoBehaviour {
 		}
 
         if (!GetComponent<AudioSource>())
-        {
-            auds = gameObject.AddComponent<AudioSource>();
-        }
+            mainTrackAuds = gameObject.AddComponent<AudioSource>();
         else
-        {
-            auds = GetComponent<AudioSource>();
-        }
+            mainTrackAuds = GetComponent<AudioSource>();
 
-		playMusic (music, 0.5f, true);
+        LoadMusicLibrary();
+
+		//playMusic (music, 0.5f, true);
     }
 
-    public void playMusic(MusicMainTrack music, float volume = 0.5f, bool loop = false)
+    void Start()
     {
-        auds.volume = volume;
-        auds.loop = loop;
-
-        auds.clip = SFXList[(int)music];
-        auds.Play();
+        mainTrackAuds.loop = true;
+        mainTrackAuds.playOnAwake = false;
     }
 
-	public void fadeMusic(float targetVolume = 0.2f, float fadeTime = 1f){
-		StartCoroutine (fadeMusicCoroutine (targetVolume, fadeTime));
-	}
-
-	private IEnumerator fadeMusicCoroutine(float targetVolume, float fadeTime){
-		bool under = true;
-
-		if (targetVolume > auds.volume) {
-			under = false;
-		}
-
-		while (auds.volume != targetVolume) {
-			auds.volume = Mathf.MoveTowards (auds.volume, targetVolume, fadeTime * Time.deltaTime);
-			Debug.Log (auds.volume);
-			yield return new WaitForEndOfFrame ();
-
-			if (under) {
-				if (auds.volume < targetVolume) {
-					auds.volume = targetVolume;
-				}
-			} else {
-				if (auds.volume > targetVolume) {
-					auds.volume = targetVolume;
-				}
-			}
-		}
-
-		auds.volume = targetVolume;
-	}
-
-    public void stopMusic(MusicMainTrack music)
+    private void LoadMusicLibrary()
     {
-        foreach (AudioSource a in gameObject.GetComponents<AudioSource>())
+        foreach(MusicTracks m in Music)
         {
-            if (a.clip == SFXList[(int)music])
+            m.musicList = Resources.LoadAll("Music/" + m.musicName, typeof(AudioClip));
+
+            for (int x = 0; x < m.musicList.Length; x++)
             {
-                if (a.isPlaying)
-                {
-                    a.Stop();
-                    a.clip = null;
-                }
+                string[] splitString = m.musicList[x].name.Split('(', ')');
+                if (splitString.Length > 1)
+                    m.musicComponents.Add(splitString[1], (AudioClip)m.musicList[x]);
+                else
+                    m.musicComponents.Add("MainTrack", (AudioClip)m.musicList[x]);
             }
         }
+
+        //Debug Code
+        //foreach (string s in Music[0].musicComponents.Keys)
+        //    Debug.Log(s);
+
+        //foreach (AudioClip ac in Music[0].musicComponents.Values)
+        //    Debug.Log(ac.name);
+    }
+
+
+    public void PlayMainTrack(string mainTrackName, float fadeTime = 0, float volume = 1f)
+    {
+        foreach(MusicTracks m in Music)
+        {
+            if(m.musicName == mainTrackName)
+            {
+                mainTrackAuds.clip = m.musicComponents["MainTrack"];
+
+                if (fadeTime != 0)
+                {
+                    StartCoroutine(c_FadeMainTrack(volume, fadeTime, true));
+                }
+                else
+                {
+                    mainTrackAuds.volume = volume;
+                    mainTrackAuds.Play();
+                }
+
+                return;
+            }
+        }
+    }
+
+    public void StopMainTrack(string mainTrackName, float fadeTime)
+    {
+        foreach (MusicTracks m in Music)
+        {
+            if (m.musicName == mainTrackName)
+            {
+                if (fadeTime != 0)
+                    StartCoroutine(c_FadeMainTrack(0, fadeTime, false));
+                else
+                    mainTrackAuds.Stop();
+
+                return;
+            }
+        }
+    }
+
+    private IEnumerator c_FadeMainTrack(float targetVol, float fadeTime, bool playMusic)
+    {
+        if (playMusic)
+            mainTrackAuds.Play();
+
+        float counter = 0;
+        while(counter < fadeTime)
+        {
+            mainTrackAuds.volume = Mathf.MoveTowards(mainTrackAuds.volume, targetVol, Time.deltaTime * fadeTime);
+            counter += Time.deltaTime / fadeTime;
+            yield return null;
+        }
+
+        mainTrackAuds.volume = targetVol;
+
+        if (!playMusic)
+            mainTrackAuds.Stop();
+    }
+
+
+    public void FadeInMusicComponent(string mainTrackName, string componentName, float fadeTime = 1f)
+    {
+        foreach(MusicTracks m in Music)
+        {
+            if (m.musicName == mainTrackName)
+            {
+                StartCoroutine(c_FadeInMusicComponent(m, componentName, fadeTime));
+                return;
+            }
+        }
+    }
+
+    private IEnumerator c_FadeInMusicComponent(MusicTracks musicTrack, string componentName, float fadeTime)
+    {
+        AudioSource auds = gameObject.AddComponent<AudioSource>();
+        musicTrack.componentsPlaying.Add(componentName, auds);
+        auds.clip = musicTrack.musicComponents[componentName];
+        auds.timeSamples = mainTrackAuds.timeSamples;
+        auds.volume = 0f;
+        auds.loop = true;
+        auds.playOnAwake = false;
+        auds.Play();
+
+        while(auds.volume < mainTrackAuds.volume)
+        {
+            auds.volume = Mathf.MoveTowards(auds.volume, mainTrackAuds.volume, Time.deltaTime * fadeTime);
+            yield return null;
+        }
+
+        auds.volume = mainTrackAuds.volume;
+    }
+
+    public void FadeOutMusicComponent(string mainTrackName, string componentName, float fadeTime = 1f)
+    {
+        foreach (MusicTracks m in Music)
+        {
+            if (m.musicName == mainTrackName)
+            {
+                StartCoroutine(c_FadeOutMusicComponent(m, componentName, fadeTime));
+                return;
+            }
+        }
+    }
+
+    private IEnumerator c_FadeOutMusicComponent(MusicTracks musicTrack, string componentName, float fadeTime)
+    {
+        AudioSource auds = musicTrack.componentsPlaying[componentName];
+
+        while (auds.volume > 0)
+        {
+            auds.volume = Mathf.MoveTowards(auds.volume, 0, Time.deltaTime * fadeTime);
+            yield return null;
+        }
+
+        auds.volume = 0;
+        musicTrack.componentsPlaying.Remove(componentName);
+        Destroy(auds);
     }
 }
