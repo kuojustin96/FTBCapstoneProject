4b6f62c5ea7e05355921aa91d7a12ad240a076c0
kuojustin96 <justinkuo3904@yahoo.com>
2018-04-13 20:10:11 -0500

    Fixed StatManager that had github errors

--- Capstone/Assets/StatManager.cs
+++ Capstone/Assets/StatManager.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -67,40 +66,3 @@ public class StatManager : MonoBehaviour {
         //Enable a ticker message
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum Stats
-{
-    SugarCollected = 0,
-    SugarStolen = 1,
-    TimeStunnedOthers = 2,
-    TimeSpentStunned = 3,
-    ItemsCraft = 4,
-    NumEmotesUsed = 5,
-    TimesVisitedBase = 6,
-    NumTimesJumped = 7,
-}
-
-public class StatManager : MonoBehaviour {
-
-    public static StatManager instance = null;
-
-    public class Stat
-    {
-        public Stats stat;
-    }
-
-	// Use this for initialization
-	void Awake () {
-        if (instance == null)
-            instance = this;
-        else
-            Destroy(this);
-	}
-
-
-}
->>>>>>> origin/master
