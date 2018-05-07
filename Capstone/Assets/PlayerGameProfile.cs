78edbb9d36f05a8cae6ed7a1995b0f5ccc6b3295
Shawn <33797022+SenorMeowzors@users.noreply.github.com>
2018-05-07 15:55:36 -0500

    Added name field that persists throughout game sessions (opening/closing the game)
    Not yet integrated into game yet

---
+++ Capstone/Assets/PlayerGameProfile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PlayerGameProfile : MonoBehaviour {
+
+    public static PlayerGameProfile profile = null;
+
+    public TMP_InputField text;
+
+    public string playerName;
+
+
+    public void UpdatePlayerName()
+    {
+        Debug.Log("Name is " + text.text);
+        playerName = text.text;
+
+        PlayerPrefs.SetString("PlayerName", playerName);
+    }
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        string theName = PlayerPrefs.GetString("PlayerName");
+        if (PlayerPrefs.GetString("PlayerName") == "")
+        {
+            playerName = "Chad";
+        }
+        else
+        {
+            playerName = theName;
+        }
+        //updates the name field;
+        text.text = playerName;
+    }
+
+}
