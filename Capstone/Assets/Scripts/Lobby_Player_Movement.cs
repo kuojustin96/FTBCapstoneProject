577b67cf95c50d71a32a570514c8dde96773bfdb
SenorMeowzors <33797022+SenorMeowzors@users.noreply.github.com>
2018-09-01 00:32:05 -0500

    Quality of life changes for the lobby

--- Capstone/Assets/Scripts/Lobby_Player_Movement.cs
+++ Capstone/Assets/Scripts/Lobby_Player_Movement.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using Cinemachine;
-
+using Prototype.NetworkLobby;
+using UnityEngine.SceneManagement;  
 public class Lobby_Player_Movement : ChadController
 {
     public override float GetGlidingFatigue()
@@ -22,5 +23,18 @@ public class Lobby_Player_Movement : ChadController
 
     protected override void LocalCameraCheck()
     {
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (!isLocalPlayer)
+            return;
+        if(SceneManager.GetActiveScene().name == LobbyManager.s_Singleton.lobbyScene)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
